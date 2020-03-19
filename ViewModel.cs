765a77def48e7a932ce97bf2824cf771aa6b5720
zean750 <zean750@naver.com>
2020-03-19 13:47:07 +0800

    ver1

---
+++ ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogsTest
+{
+    public class ViewModel
+    {
+        private IDialogCoordinator dialogCoordinator;
+
+
+        public ViewModel(IDialogCoordinator instance)
+        {
+            dialogCoordinator = instance;
+
+            GetDialog();
+        }
+
+        public async void GetDialog()
+        {
+            var message = await dialogCoordinator.ShowProgressAsync(this, "Please", "载入中,请稍后...");
+            message.SetIndeterminate();
+            await Task.Delay(5000);
+
+            await message.CloseAsync();
+        }
+    }
+}
